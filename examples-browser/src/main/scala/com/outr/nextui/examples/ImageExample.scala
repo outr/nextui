f31e3a9497404c4ec015f079e1d9ef2908861f99
Matt Hicks <matt@outr.com>
2016-06-01 19:08:23 -0500

    Minor updates to Scala.js examples.

--- examples-browser/src/main/scala/com/outr/nextui/examples/ImageExample.scala
+++ examples-browser/src/main/scala/com/outr/nextui/examples/ImageExample.scala
@@ -8,7 +8,7 @@ import pl.metastack.metarx._
 object ImageExample extends UI with ScalaJS {
   title := "Image Example"
 
-  lazy val img1 = Image("https://puppydogweb.com/gallery/puppies/labradorretriever2.jpg")
+  lazy val img1 = Image("images/tucker.jpg")
   lazy val img2 = Image("https://img.buzzfeed.com/buzzfeed-static/static/2014-07/28/10/enhanced/webdr11/enhanced-12094-1406557546-35.jpg")
   val imageView = new ImageView {
     image := img1
--- examples-browser/src/main/scala/com/outr/nextui/examples/VirtualSizeExample.scala
+++ examples-browser/src/main/scala/com/outr/nextui/examples/VirtualSizeExample.scala
@@ -5,15 +5,15 @@ import com.outr.nextui.model.Image
 import com.outr.nextui.{ImageView, UI, VirtualMode, VirtualSizeSupport}
 
 object VirtualSizeExample extends UI with ScalaJS with VirtualSizeSupport {
-  virtualWidth := 450.0
-  virtualHeight := 338.0
+  virtualWidth := 1024.0
+  virtualHeight := 768.0
   virtualMode := VirtualMode.Bars
 
   children += new ImageView {
-    image := Image("https://puppydogweb.com/gallery/puppies/labradorretriever2.jpg")
+    image := Image("images/1024.jpg")
     x := 0.0.vx
     y := 0.0.vy
-    width := 450.0.vw
-    height := 338.0.vh
+    width := 1024.0.vw
+    height := 768.0.vh
   }
 }
