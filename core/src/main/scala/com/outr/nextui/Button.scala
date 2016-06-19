e08f8441713f038480ce16620c40bd0f46bd896d
Matt Hicks <matt@outr.com>
2016-06-18 21:11:04 -0500

    Updated HelloWorld to use Label instead of Button and deprecated Button. It should be replaced later with a custom component so it isn't implementation dependent.

--- core/src/main/scala/com/outr/nextui/Button.scala
+++ core/src/main/scala/com/outr/nextui/Button.scala
@@ -3,6 +3,7 @@ package com.outr.nextui
 import com.outr.nextui.event.ActionEvent
 import pl.metastack.metarx.{Channel, Sub}
 
+@deprecated
 class Button extends Component {
   lazy val text: Sub[String] = Sub("")
 
--- examples/src/main/scala/com/outr/nextui/examples/HelloWorld.scala
+++ examples/src/main/scala/com/outr/nextui/examples/HelloWorld.scala
@@ -1,19 +1,15 @@
 package com.outr.nextui.examples
 
-import com.outr.nextui.{Button, UI}
+import com.outr.nextui.{Label, UI}
 
 object HelloWorld extends UI {
-  title := "Hello World"
+  title := "Hello World Example"
   width.pref := 300.0
   height.pref := 250.0
 
-  children += new Button {
-    text := "Say 'Hello World'"
+  children += new Label {
+    text := "Hello World"
     center := ui.center
     middle := ui.middle
-
-    action.attach { evt =>
-      logger.info("Hello World!")
-    }
   }
 }
